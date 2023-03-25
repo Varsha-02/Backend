1c7abc3aa3c2549ce1994efa29e28de6ad91e411
msvarsha02 <msvarsha02@gmail.com>
2023-03-25 22:48:22 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -2,24 +2,48 @@
 const express = require('express');
 const app = express();
 
+
 const mongoose = require('mongoose');
 const login = require('./models/login')
 const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-const mongodbpath = "mongodb+srv://msvarsha02:Varsha2820@cluster0.7d53sa6.mongodb.net/appdb";
-mongoose.connect(mongodbpath).then(function()
+mongoose.connect("mongodb+srv://msvarsha02:Varsha2820@cluster0.7d53sa6.mongodb.net/appdb").then(function()
 {
     app.get("/",function(req, res){
         const response = { message: "API Works" };
         res.json(response);
     });
-    const loginrouter = require("./routes/login");
-    app.use("./login", loginrouter);
+    // const loginrouter = require("./routes/login");
+    // app.use("./login", loginrouter);
+    app.post("/login/list/",async function(req, res){
+        var val =  await login.find( );
+        res.json(val);
+    });
+    
+    app.post("/login/add",async function(req, res){
+       await login.deleteOne({id: req.body.id});
+       const newval = new login({
+        id: req.body.id,
+        Name: req.body.Name,
+        Phno: req.body.Phno,
+        Email: req.body.Email
+       });
+        await newval.save();
+        const response = { message: "User Added"};
+        res.json(response);
+    });
+    
+    app.post("/login/delete", async function(req, res) {
+        await login.deleteOne({id: req.body.id});
+        const response = { message: "User Deleted"};
+        res.json(response);
+    
+    }) ;
 });
 
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, function() {
-    console.log("Server started at PORT : " + PORT);
+
+app.listen(5000, function() {
+    console.log("Server started at PORT : 5000");
 });
